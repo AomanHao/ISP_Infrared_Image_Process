8d29a1d5e523831cc95cab3515f5fd4e5e0ec845
aomanhao <1014325364@qq.com>
2022-12-06 23:20:07 +0800

    add sharpe

--- DetailEnhance/Demo_DetailEnhance.m
+++ DetailEnhance/Demo_DetailEnhance.m
@@ -1,4 +1,4 @@
-%% 程序分享 
+%% 程序分享
 % 个人博客 www.aomanhao.top
 % Github https://github.com/AomanHao
 % CSDN https://blog.csdn.net/Aoman_Hao
@@ -17,12 +17,13 @@ img_name = {img_conf.name};
 img_num = numel({img_conf.name})-2;
 
 data_type = 'bmp'; % raw: raw data
-                    %bmp: bmp data
+%bmp: bmp data
 conf.savepath = './result/';
 if ~exist(conf.savepath,'var')
     mkdir(conf.savepath)
 end
 
+method = 'Super_Sharpe';
 conf.remake=[];
 for i = 1:img_num
     switch data_type
@@ -30,22 +31,27 @@ for i = 1:img_num
             name = split(img_name{i+2},'.');
             conf.imgname = name{1};
             conf.imgtype = name{2};
-            img_re = imread([pathname,img_name{i+2}]);
-            img = im2double(img_re);
-            [m_img,n_img,z_img] = size(img);
-            figure;imshow(img);
+            img = imread([pathname,img_name{i+2}]);
+            img_in = im2double(img);
+            [m_img,n_img,z_img] = size(img_in);
+            figure;imshow(img_in);
     end
-
-    %% enhance
+    
+    if z_img == 3
+        img_yuv = rgb2ycbcr(img_in);
+        img_y = img_yuv(:,:,1);
+    else
+        img_y = img_in;
+    end
+    
+    %% Single channel data only
+    switch method
+        case 'Super_Sharpe'
+            conf.w = 0.8;
+            d_out = super_sharpness_aomanhao(img_y,conf);
+            
+    end
     
     
-    
-    %% result save
-    imwrite(uint8(enhan_img),strcat(conf.savepath,conf.name,'_',enhan_type,'-',num2str(t),'_',conf.remake,'.png'));
-    
+    figure;imshow(d_out);
 end
-
-
-
-
-
